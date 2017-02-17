4ae4dda9078ebfc48f7072cfb7673d04ed948edd
ghren <alex1212112@gmail.com>
2017-02-17 20:11:39 +0800

    数据库插入用户信息

--- dbmanager.js
+++ dbmanager.js
@@ -2,15 +2,56 @@ var dbmanager = {};
 module.exports = dbmanager;
 
 var mongoose = require("mongoose");
-
 var db = mongoose.connection;
-
 db.on('error',console.error);
+
+// 发布数据库的 UserModel
+var userSchema  = new mongoose.Schema({
+        username : {type : String},
+        password : {type : String},
+        uid : Number
+});
+
+var userModel = db.model('User',userSchema);
+
+
 
 
 dbmanager.connect = function(){
   mongoose.connect('mongodb://localhost/test');
 };
+
+dbmanager.insertUser = function (name,pwd,userId) {
+
+     var user = this.dbUserWithParameters(name,pwd,userId);
+     user.save(function (err,user) {
+        if (err) {
+            console.log(err);
+        }
+        console.log("user did saved");
+     });
+};
+
+
+dbmanager.dbUserWithParameters = function (name,pwd,userId) {
+      var user = new userModel({
+           username : name,
+           password : pwd,
+           uid : userId
+      });
+      return user;
+};
+
+// dbmanager.userSchema  = new mongoose.Schema({
+//         username : {type : String},
+//         password : {type : String},
+//         uid : Number
+// });
+
+dbmanager.UserModel = db.model('User',this.userSchema);
+
+
+
 
 dbmanager.insertData = function () {
   db.once('open',function(){
--- dbtest.js
+++ dbtest.js
@@ -1,3 +1,5 @@
 var dbmanager = require("./dbmanager.js");
 dbmanager.connect();
-dbmanager.insertData();
+// dbmanager.insertData();
+
+dbmanager.insertUser("renguohua","123456",121223242343242);
