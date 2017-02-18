a64b53c1eeb4361a7e87ba6a1bc053f0fe42f5d9
ghren <alex1212112@gmail.com>
2017-02-18 17:05:08 +0800

    修改router

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -6,8 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/login', function(req, res, next) {
-  res.render('login');
+router.route('/login').get(function(req, res) {
+		res.render('login');
+}).post(function(req, res) {
+
 });
+
+router.route('/register').get(function(req, res) {
+		res.render('register');
+}).post(function(req, res) {
+     console.log(req.body);
+     res.send('test');
+});
+
+
+
 
 module.exports = router;
