36987957e723bf468f2521ad6c5bb30c9ae3fb1c
ghren <alex1212112@gmail.com>
2017-02-18 16:37:32 +0800

    增加数据库表结构

--- db/dbtest.js
+++ db/dbtest.js
@@ -2,4 +2,11 @@ var dbmanager = require("./dbmanager.js");
 dbmanager.connect();
 // dbmanager.insertData();
 
-dbmanager.insertUser("renguohua","123456",121223242343242);
+dbmanager.insertUser("wangzheHarray","123456",1212242343242,function(err,user){
+
+    if(err){
+        console.log(err);
+        return;
+    }
+    console.log("user did saved");
+});
