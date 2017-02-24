af260f56fb6ed3e905da821d116ce0fe5222c89c
ghren <alex1212112@gmail.com>
2017-02-25 00:11:42 +0800

    完成设置任务接口调试，后续考虑如何使用 Promise 的方式

--- myapp/routes/configRoute.js
+++ myapp/routes/configRoute.js
@@ -1,19 +1,64 @@
-var express = require('express');
-var router = express.Router();
-var db = require('../db/dbmanager')
+const express = require('express');
+const db = require('../db/dbmanager')
+
+const router = express.Router();
 
 
 router.get('/', (req, res) => {
-    res.render('config', {warn: undefined})
-})
+    res.render('config', { warn: undefined });
+});
 
 
 router.post('/', (req, res) => {
+    console.log(`config : ${JSON.stringify(req.body)}`);
+    db.findOneUserWithUsername(req.session.username,(error,user) => {
 
-  console.log("config : "+ JSON.stringify(req.body));
+        let tasks = convertConfigModelToDBModel(req.body);
+        let taskNumber = 0;
+        tasks.forEach((value, index, array) => {
+            saveTask(user.uid, value, (error,task) => {
+          });
+          const data = req.body;
+          data.msg = '提交成功';
+          res.render('config', data);
+      });
+    });
+});
 
-  res.render('config',{msg:"提交成功"});
+function convertConfigModelToDBModel(configModel) {
+    let startTime = new Date(configModel.start);
+    let endTime = new Date(configModel.end);
+    let startTimestamp = startTime.getTime();
+    let endTimestamp = endTime.getTime();
+    let days = (endTimestamp - startTimestamp) / (60 * 60 * 24 * 1000);
+    let tasks = [];
+    let hours = [];
+    for (let i = 0; i < 24; i += 1) {
+        let hour = `value${i}`;
+        if (configModel[hour].length > 0) {
+          hours.push(i);
+        }
+    }
+    for (let n = 0; n < days; n += 1) {
+        startTime.setDate(n);
+        for (let m = 0; m < hours.length; m += 1) {
+            startTime.setHours(hours[m]);
+            let task = {};
+            task.platformName = configModel.name;
+            task.appName = configModel.appname;
+            task.appUserName = configModel.appusrname;
+            task.appUserPwd = configModel.apppwd;
+            task.dt = startTime.getTime();
+            task.price = configModel[`value${hours[m]}`];
+            tasks.push(task);
+        }
+    }
+    return tasks;
+}
 
-})
+function saveTask(uid, task, handler) {
+    db.insertTask(uid, task.platformName, task.appUserName, task.appUserPwd,
+      task.appName, task.dt, task.price, true, handler);
+}
 
 module.exports = router;
