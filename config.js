9ba8324426ff881fb5ebfbe325256a3e7c19d63b
王哲 <wangzheharry@didichuxing.com>
2017-02-27 11:07:30 +0800

    修改config

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ config = {};
 
 config.filesForCommunication = ['result.txt','checkCodeText.txt','login.jpg','before.jpg','after.jpg','checkCode.jpg','checkCodeFiltered.jpg','alertedMessage.txt'];
 
-config.captchaConfig={'username':'w390467556','pwd':'wz13820367061','typeid':'2040','softid': '58316','softkey': '857679ab35f94d4aa472e5be50a171bb'};
+config.captchaConfig={'username':'','pwd':'','typeid':'','softid': '58316','softkey': '857679ab35f94d4aa472e5be50a171bb'};
 
-config.oppoUrl={'login':'http://open.oppomobile.com/newuser/login','elogin':'http://e.oppomobile.com/login','distribute':'http://e.oppomobile.com/bid/list','search':'http://e.oppomobile.com/searchBid/list','expand':'http://e.oppomobile.com/feeds/list'};
+config.oppoUrl={'login':'','elogin':'','distribute':'','search':'','expand':''};
 
 config.filePath={'checkCodeJpg':'checkCode.jpg','checkCodeText':'checkCodeText.txt'};
 
